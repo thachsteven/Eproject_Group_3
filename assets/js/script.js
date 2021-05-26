8e2475a83e53b417f2fac8c8afe3a7be8cd21e67
Thach Steven <thachsteven.marketing@gmail.com>
2021-05-26 21:19:02 +0700

    commit
    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -108,3 +108,11 @@ function checkInputsFeedback() {
 
 
 }
+
+
+$(document).ready(function() {
+        $("#name").on('change', function(){
+            $(".data").hide();
+            $("#" + $(this).val()).fadeIn(700);
+        }).change();
+    });
